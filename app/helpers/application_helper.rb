3a3e24ebf4130c71f1df2e35696b0d16d2f4f44d
James Keane <jpkeane@me.com>
2017-06-07 18:48:51 +0100

    Added title helper spec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = 'FloworxSA'
+    base_title = 'Floworx SysArch'
     if page_title.empty?
       base_title
     else
