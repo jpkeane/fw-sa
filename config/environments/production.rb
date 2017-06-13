bf7e4f99bbd6af427ecf13a4efbaea5c15aeb0c8
James Keane <jpkeane@me.com>
2017-06-13 01:48:15 +0100

    Added production domains

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fw-sysarch-calliope.herokuapp.com'
+  host = 'fw-sa-calliope.jpkeane.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       address:              'smtp.sendgrid.net',
@@ -96,7 +96,7 @@ Rails.application.configure do
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               'heroku.com',
+      domain:               'jpkeane.com',
       enable_starttls_auto: true
   }
 
